8f274e3a29662c64449933b03c6c11f898c3c9a1
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2021-09-08 12:21:36 +0300

    mapView added

---
+++ LandmarkBookWithSwiftUI/Models/LandmarkModel.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkModel.swift
+//  LandmarkBookWithSwiftUI
+//
+//  Created by Semih Kalaycı on 8.09.2021.
+//
+import SwiftUI
+import CoreLocation
+
+struct LandmarkModel: Identifiable {
+    var id : Int
+    var name : String
+    var imageName: String
+    var country: String
+    var category: String
+    var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D{
+        CLLocationCoordinate2D(latitude: coordinates.lat, longitude: coordinates.long)
+    }
+}
+
+struct Coordinates {
+    var lat: Double
+    var long: Double
+}
+
+
+let eiffelLandmark = LandmarkModel(id: 0, name: "Eiffel", imageName: "eiffel", country: "France", category: "Tower", coordinates: Coordinates(lat: 48.858093, long: 2.294694))
+
+let pissaandmark = LandmarkModel(id: 1, name: "Pissa", imageName: "pissa", country: "Italy", category: "Tower", coordinates: Coordinates(lat: 43.7230, long: 10.3966))
+
+
+let landmarkArray = [eiffelLandmark,pissaandmark]
--- LandmarkBookWithSwiftUI/Views/MapView.swift
+++ LandmarkBookWithSwiftUI/Views/MapView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 import MapKit
 
-struct MapView: View {
+struct MapView: UIViewRepresentable {
+    
     var coordinate : CLLocationCoordinate2D
-    func updateUIView(_ uiView: MKMapView, context: Context) {
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
         let region = MKCoordinateRegion(center: coordinate, span: span)
-        uiView.setRegion(region, animated:true)
+        uiView.setRegion(region,animated: true)
     }
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
-    }
-}
+    }}
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
